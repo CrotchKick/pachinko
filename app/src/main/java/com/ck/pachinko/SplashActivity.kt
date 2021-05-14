554fd3a46815a4bc581a6a0510e4368715cc87e9
Masashi Miyoshi <miyoshi@ak-plus.co.jp>
2021-05-14 09:48:13 +0900

    commit

--- app/src/main/java/com/ck/pachinko/SplashActivity.kt
+++ app/src/main/java/com/ck/pachinko/SplashActivity.kt
@@ -3,13 +3,12 @@ package com.ck.pachinko
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.*
 
 class SplashActivity : AppCompatActivity() {
     //インターステイシャル
-    private lateinit var mInterstitialAd: InterstitialAd
+//    private lateinit var mInterstitialAd: InterstitialAd
     //インターステイシャル
 
     val handler = Handler()
@@ -24,16 +23,16 @@ class SplashActivity : AppCompatActivity() {
             "ca-app-pub-3940256099942544~3347511713"
         }
         //インターステイシャル
-        mInterstitialAd = InterstitialAd(this)
-        mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
-        mInterstitialAd.loadAd(AdRequest.Builder().build())
+//        mInterstitialAd = InterstitialAd(this)
+//        mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
+//        mInterstitialAd.loadAd(AdRequest.Builder().build())
         //インターステイシャル
 
     }
     override fun onResume() {
         super.onResume()
         //2000ミリ秒遅れて、「spHandler」を実行させる
-        handler.postDelayed(spHandler, 300)
+        handler.postDelayed(spHandler, 1000)
     }
     override fun onStop() {
         super.onStop()
@@ -46,48 +45,48 @@ class SplashActivity : AppCompatActivity() {
         override fun run() {
 
 /////////////初期リリースはこちらをコメント外してインターステイシャル無しでもよい
-//            intent = Intent(this@SplashActivity, MainActivity::class.java)
-//            startActivity(intent)
-//            //画面遷移アクション無し
-//            overridePendingTransition(0, 0)
-//            //アクティビティを破棄する
-//            this@SplashActivity.finish()
+            intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            //画面遷移アクション無し
+            overridePendingTransition(0, 0)
+            //アクティビティを破棄する
+            this@SplashActivity.finish()
 /////////////初期リリースはこちらをコメント外してインターステイシャル無しでもよい
 
 /////////////ここから下インターステイシャルが表示される。
-            mInterstitialAd.adListener = object: AdListener() {
-                override fun onAdLoaded() {
-                    // Code to be executed when an ad finishes loading.
-                    // 広告を読み込んだら広告を表示、それまではスプラッシュ画面
-                    if (mInterstitialAd.isLoaded) {
-                        mInterstitialAd.show()
-                    } else {
-                        Log.d("TAG", "The interstitial wasn't loaded yet.")
-                    }
-                }
-
-                override fun onAdFailedToLoad(errorCode: Int) {
-                    // Code to be executed when an ad request fails.
-                    // インターネットにつながらない環境で接続してきた場合は、メインアクティビティに移動
-                    intent = Intent(this@SplashActivity, MainActivity::class.java)
-                    startActivity(intent)
-                    //画面遷移アクション無し
-                    overridePendingTransition(0, 0)
-                    //アクティビティを破棄する
-                    this@SplashActivity.finish()
-                }
-
-                override fun onAdClosed() {
-                    // Code to be executed when the interstitial ad is closed.
-                    // インターステイシャル広告を削除、戻るボタン押したらメインアクティビティに移動
-                    intent = Intent(this@SplashActivity, MainActivity::class.java)
-                    startActivity(intent)
-                    //画面遷移アクション無し
-                    overridePendingTransition(0, 0)
-                    //アクティビティを破棄する
-                    this@SplashActivity.finish()
-                }
-            }
+//            mInterstitialAd.adListener = object: AdListener() {
+//                override fun onAdLoaded() {
+//                    // Code to be executed when an ad finishes loading.
+//                    // 広告を読み込んだら広告を表示、それまではスプラッシュ画面
+//                    if (mInterstitialAd.isLoaded) {
+//                        mInterstitialAd.show()
+//                    } else {
+//                        Log.d("TAG", "The interstitial wasn't loaded yet.")
+//                    }
+//                }
+//
+//                override fun onAdFailedToLoad(errorCode: Int) {
+//                    // Code to be executed when an ad request fails.
+//                    // インターネットにつながらない環境で接続してきた場合は、メインアクティビティに移動
+//                    intent = Intent(this@SplashActivity, MainActivity::class.java)
+//                    startActivity(intent)
+//                    //画面遷移アクション無し
+//                    overridePendingTransition(0, 0)
+//                    //アクティビティを破棄する
+//                    this@SplashActivity.finish()
+//                }
+//
+//                override fun onAdClosed() {
+//                    // Code to be executed when the interstitial ad is closed.
+//                    // インターステイシャル広告を削除、戻るボタン押したらメインアクティビティに移動
+//                    intent = Intent(this@SplashActivity, MainActivity::class.java)
+//                    startActivity(intent)
+//                    //画面遷移アクション無し
+//                    overridePendingTransition(0, 0)
+//                    //アクティビティを破棄する
+//                    this@SplashActivity.finish()
+//                }
+//            }
 /////////////ここから上のコメントアウトを外したらインターステイシャルが表示される。
         }
     }
